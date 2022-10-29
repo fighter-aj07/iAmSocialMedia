f3bea6af799a6ed3b2587dc29213d6f61701f53a
APPLE <appleblade22@gmail.com>
2022-10-29 14:23:10 +0530

    frontend login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Homepage from "./components/Home/Homepage";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Signup />
     </div>
   );
 }
