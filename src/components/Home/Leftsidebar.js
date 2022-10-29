e6f1a052a3314146f3157b2f84e20624d9742d70
Meet Jain <93723420+MeetJain20@users.noreply.github.com>
2022-10-29 11:01:49 +0530

    homepage split into components

---
+++ src/components/Home/Leftsidebar.js
@@ -0,0 +1,130 @@
+import React from "react";
+import "./Leftsidebar.css";
+
+const Leftsidebar = () => {
+  return (
+    <div className="left">
+      <ul className="leftside">
+        <li className="leftitems">
+          <i className="iconsss fa-solid fa-message"></i>
+          <span className="leftitemsname">Chats</span>
+        </li>
+        <li className="leftitems">
+          <i className="iconsss fa-regular fa-rss"></i>
+          <span className="leftitemsname">Feed</span>
+        </li>
+        <li className="leftitems">
+          <i className="iconsss fa-regular fa-video"></i>
+          <span className="leftitemsname">Videos</span>
+        </li>
+        <li className="leftitems">
+          <i className="iconsss fa-regular fa-user-group"></i>
+          <span className="leftitemsname">Groups</span>
+        </li>
+        <li className="leftitems">
+          <i className="iconsss fa-regular fa-calendar"></i>
+          <span className="leftitemsname">Events</span>
+        </li>
+        <li className="leftitems quest">
+          <i className="iconsss fa-regular fa-question"></i>
+          <span className="leftitemsname">Questions</span>
+        </li>
+      </ul>
+      <ul className="leftsidebottom">
+        <li className="leftitems friends">Friends</li>
+        <li className="leftitemsbottom">
+          <div className="imgsrc">
+            <img
+              src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+              alt="Loading"
+              className="profimg"
+            />
+            <span className="leftitemsname2">Meet Jain</span>
+          </div>
+        </li>
+        <li className="leftitemsbottom">
+          <div className="imgsrc">
+            <img
+              src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+              alt="Loading"
+              className="profimg"
+            />
+            <span className="leftitemsname2">Meet Jain</span>
+          </div>
+        </li>
+        <li className="leftitemsbottom">
+          <div className="imgsrc">
+            <img
+              src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+              alt="Loading"
+              className="profimg"
+            />
+            <span className="leftitemsname2">Meet Jain</span>
+          </div>
+        </li>
+        <li className="leftitemsbottom">
+          <div className="imgsrc">
+            <img
+              src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+              alt="Loading"
+              className="profimg"
+            />
+            <span className="leftitemsname2">Meet Jain</span>
+          </div>
+        </li>
+        <li className="leftitemsbottom">
+          <div className="imgsrc">
+            <img
+              src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+              alt="Loading"
+              className="profimg"
+            />
+            <span className="leftitemsname2">Meet Jain</span>
+          </div>
+        </li>
+        <li className="leftitemsbottom">
+          <div className="imgsrc">
+            <img
+              src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+              alt="Loading"
+              className="profimg"
+            />
+            <span className="leftitemsname2">Meet Jain</span>
+          </div>
+        </li>
+        <li className="leftitemsbottom">
+          <div className="imgsrc">
+            <img
+              src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+              alt="Loading"
+              className="profimg"
+            />
+            <span className="leftitemsname2">Meet Jain</span>
+          </div>
+        </li>
+        <li className="leftitemsbottom">
+          <div className="imgsrc">
+            <img
+              src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+              alt="Loading"
+              className="profimg"
+            />
+            <span className="leftitemsname2">Meet Jain</span>
+          </div>
+        </li>
+        <li className="leftitemsbottom">
+          <div className="imgsrc">
+            <img
+              src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+              alt="Loading"
+              className="profimg"
+            />
+            <span className="leftitemsname2">Meet Jain</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Leftsidebar;
---
+++ src/components/Home/Rightsidebar.js
@@ -0,0 +1,96 @@
+import React from "react";
+import "./Rightsidebar.css";
+
+const Rightsidebar = () => {
+  return (
+    <div className="right">
+      <div className="righttop">
+        <div className="firstt">
+          <div className="firsttleft">
+            <img
+              src="https://tse4.mm.bing.net/th?id=OIP.RpBo_sBCV3Kpq2P3CSdKIAHaHa&pid=Api&P=0"
+              alt="Loading"
+              className="firsttleftimg"
+            />
+          </div>
+          <div className="firsttright">
+            <span className="fw-bold">Meet Jain</span>
+            <span> and </span> <span className="fw-bold">3 other friends </span>
+            <span>have their Birthday today !</span>
+          </div>
+        </div>
+        <div className="lastt">
+          <img
+            src="https://tse2.mm.bing.net/th?id=OIP.3QqovxjOUHIl5oynJPYdqgHaEJ&pid=Api&P=0"
+            alt="Loading"
+            className="rightimgtop"
+          />
+        </div>
+      </div>
+      <div className="rightbottom">
+        <ul className="rightside">
+          <li className="rightitems">
+            <p className="onlinefr">Online Friends</p>
+          </li>
+          <li className="rightitems">
+            <div className="imgsrc">
+              <div className="onli"></div>
+              <img
+                src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+                alt="Loading"
+                className="profimg"
+              />
+              <span className="rightitemsname2">Meet Jain</span>
+            </div>
+          </li>
+          <li className="rightitems">
+            <div className="imgsrc">
+              <div className="onli"></div>
+              <img
+                src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+                alt="Loading"
+                className="profimg"
+              />
+              <span className="rightitemsname2">Meet Jain</span>
+            </div>
+          </li>
+          <li className="rightitems">
+            <div className="imgsrc">
+              <div className="onli"></div>
+              <img
+                src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+                alt="Loading"
+                className="profimg"
+              />
+              <span className="rightitemsname2">Meet Jain</span>
+            </div>
+          </li>
+          <li className="rightitems">
+            <div className="imgsrc">
+              <div className="onli"></div>
+              <img
+                src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+                alt="Loading"
+                className="profimg"
+              />
+              <span className="rightitemsname2">Meet Jain</span>
+            </div>
+          </li>
+          <li className="rightitems">
+            <div className="imgsrc">
+              <div className="onli"></div>
+              <img
+                src="https://tse3.mm.bing.net/th?id=OIP.WkuGv4-iR5uPKZFcs7UjvAHaHs&pid=Api&P=0"
+                alt="Loading"
+                className="profimg"
+              />
+              <span className="rightitemsname2">Meet Jain</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Rightsidebar;
