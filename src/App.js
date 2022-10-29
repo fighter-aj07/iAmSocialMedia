21ae4564cfb8db5a2460349bb13cd8a766ee7f63
APPLE <appleblade22@gmail.com>
2022-10-29 18:16:47 +0530

    Routing done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import Homepage from "./components/Home/Homepage";
 import Profile from "./components/Profile/Profile"
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Signup />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+
+
   );
 }
 
