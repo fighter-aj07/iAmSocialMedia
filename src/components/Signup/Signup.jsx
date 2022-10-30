5dac1a4f8516a68c7d50ba253b1332ea62a60f46
APPLE <appleblade22@gmail.com>
2022-10-30 17:09:46 +0530

    Almost done login and signup

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,6 +1,76 @@
 import "./signup.css";
+import {useState} from "react";
+import login from "../../Database/login";
+import { Link } from "react-router-dom";
+import bcrypt from "bcryptjs";
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Signup() {
+    const [email, setEmail] = useState("");
+    const [fetching, setFetching] = useState(false);
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [isError, setisError] = useState(false);
+    const [error, setError] = useState("");
+    const [passwordAgain, setPasswordAgain] = useState("");
+
+    const validateEmail = (email) => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const handleClick = (e) => {
+        setFetching(true);
+        e.preventDefault();
+        if(email && password && username && passwordAgain){
+            if(validateEmail(email)){
+                if(password === passwordAgain){
+                    setError("");
+                    setisError(false);
+                    const hashedPassword = bcrypt.hashSync(password, "$2a$10$CwTycUXWue0Thq9StjUM0u");
+                    //if email or username already exists in database then show error
+                    const user = login.find((user) => user.email === email);
+                    if(user){
+                        setError("Email already exists");
+                        setisError(true);
+                    }
+                    else{
+                    //hash password and save to database
+                    console.log(hashedPassword);
+                    //save to database
+                    //insert into login object
+                    //generate userid from time
+                    const userid = Date.now() + Math.random();
+                    login.push({
+                        "userid": userid,
+                        "username": username,
+                        "password": hashedPassword,
+                        "email": email
+                    });
+                    //set user to local storage
+                    localStorage.setItem("user", JSON.stringify(
+                        {
+                            "userid": userid,
+                        }
+                    ));
+
+                    window.location.replace("/profile");
+                }
+            }
+                else{
+                    setError("Passwords do not match");
+                    setisError(true);
+                }
+            }
+            else{
+                setError("Invalid email");
+                setisError(true);
+            }
+        }   
+
+        setFetching(false);
+    }
+
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -12,14 +82,58 @@ export default function Signup() {
                 </div>
                 <div className="loginRight">
                     <div className="loginBox">
-                        <input placeholder="Username" className="loginInput" />
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" className="loginInput" />
-                        <input placeholder="Password Again" className="loginInput" />
-                        <button className="loginButton">Sign Up</button>
+                        <input 
+                        placeholder="Username" 
+                        className="loginInput" 
+                        required
+                        type="text"
+                        onChange={(e) => {
+                            setUsername(e.target.value);
+                            setError("");
+                        }}/>
+                        <input 
+                        placeholder="Email" 
+                        className="loginInput"
+                        required
+                        type="email"
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                            setError("");
+                        }}
+                        />
+                        <input placeholder="Password" 
+                        className="loginInput" 
+                        required
+                        type="password"
+                        minLength="6"
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                            setError("");
+                        }}/>
+                        <input placeholder="Password Again" 
+                        className="loginInput" 
+                        required
+                        type="password"
+                        minLength="6"
+                        onChange={(e) => {
+                            setPasswordAgain(e.target.value);
+                            setError("");
+                        }}/>
+                        {isError && <span className="error">{error}</span>}
+
+                        <button className="loginButton"
+                        onClick={handleClick}
+                        type="submit"
+                        disabled={fetching}
+                        >
+                            {fetching ? <CircularProgress color="inherit" size="20px" /> : "Sign Up"}
+                        </button>
+
+                        <Link to="/login" style = {{textDecoration: "none",textAlign: "center"}}>
                         <button className="loginRegisterButton">
                             Log into Account
                         </button>
+                        </Link>
                     </div>
                 </div>
             </div>
